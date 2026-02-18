3d6133f130738e3052c251f24afa4d7b3816daf8
Azda45 <growtopiagame875236@gmail.com>
2026-02-18 22:07:40 +0800

    add | favicon

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // ─── Dashboard ────────────────────────────────────────────────────────────
 app.get("/", async (req, res) => {
